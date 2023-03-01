c62ca866417a3605767c14a674791dadcf92a07d
Juan <mtzjuanmtz27@gmail.com>
2023-03-01 12:54:04 -0600

    24/02/2023
    git status
    git commit -m 24/02/2023

--- app.py
+++ app.py
@@ -1,25 +1,35 @@
-from flask import Flask, request, render_template, redirect
-
+from flask import Flask,render_template,request
+from flask import flash,make_response
 import forms
-from clases import Calculadora
+import traductor
+from flask_wtf.csrf import CSRFProtect
+from flask import Flask, request, render_template, redirect
+from CajasDinamicas import Calculadora
 
-app = Flask(__name__)
+app = Flask (__name__)
+app.config['SECRET_KEY']="Esta es una clave encriptada"
+csrf=CSRFProtect()
+
+@app.route("/formprueba")
+def formprueba():
+  
+    return render_template("formprueba.html")
+
+@app.route("/Alumnos",methods=['GET','POST'])
+def Alumnos():
+    reg_alum=forms.UserForm(request.form)
+    datos = list()
+    if request.method == 'POST' and reg_alum.validate():
+        datos.append(reg_alum.matricula.data)
+        datos.append(reg_alum.nombre.data)
+        print(reg_alum.matricula.data)
+        print(reg_alum.nombre.data)
+    return render_template("Alumnos.html", form=reg_alum,datos=datos)
+
 
 @app.route('/')
 def mainn():
     return redirect('/cajas_dinamica')
-
-@app.route('/alumnos', methods=['GET', 'POST'])
-def alumnos():
-    registrar_Alumno = forms.UserForm(request.form)
-    Mat = ''
-    Nom = ''
-        
-    if request.method == 'POST':
-        Mat = registrar_Alumno.matricula.data
-        Nom = registrar_Alumno.nombre.data
-    
-    return render_template('alumnos.html', form = registrar_Alumno, mat=Mat, nom = Nom)
 
 @app.route('/cajas_dinamica', methods=['GET', 'POST'])
 def cajas_dinamica(): 
@@ -47,20 +57,57 @@ def calcular():
                            nMayor = Calculadora.num_Mayor(Numeros),
                            name = "Resultado de " + str(len(Numeros)) + " números")
 
+@app.route("/traductor", methods=['GET','POST'])
+def traducir():
+    req_in = traductor.traducirIn(request.form)
+    req_out = traductor.traducirOut(request.form)
+    datos = list()
+    pal2 = ''
+    if request.method == 'POST':
+        if req_out.envio.data == 'tra' and req_out.validate():
+            palabra = ''
+            for d in req_out.pal.data:
+                palabra += d.lower()
+
+            e=open('traductor_esp.txt','r')
+            i=open('traductor_ing.txt','r')
+            dicionarioE = e.readlines()
+            dicionarioI = i.readlines()
+
+            if req_out.idioma.data == 'esp':
+                for item in dicionarioE:
+                    if palabra in item:
+                        pal2 += dicionarioI[dicionarioE.index(item)]+', '
+            else:
+                for item in dicionarioI:
+                    if palabra in item:
+                        pal2 += dicionarioE[dicionarioI.index(item)]+', '
+                        
+            e.close()
+            i.close()
+            datos.append('La palabra traducida fue: "'+palabra+'"')
 
 
+        if req_in.envio.data == 'gua' and req_in.validate():
+            esp = ''
+            ing = ''
+            for d in req_in.esp.data:
+                esp += d.lower()
+            for d in req_in.ing.data:
+                ing += d.lower()
+            datos.append('Palabra guardada (esp) '+esp)
+            datos.append('Palabra guardada (ing) '+ing)
+            e=open('traductor_esp.txt','a')
+            i=open('traductor_ing.txt','a')
+            e.write(esp+'\n')
+            i.write(ing+'\n')
+            e.close()
+            i.close()
 
-    # Numeros = Calculadora.get_Array(request.form)
+    return render_template("traductor.html",formIn=req_in,formOut=req_out, datos=datos,pal2 = pal2)
 
-    # return render_template("OtroCajasDinamicas.html",
-    #                        numeros = Numeros,
-    #                        repetidos = Calculadora.repeticiones(Numeros),
-    #                        comas = Calculadora.concatenar_Numeros(Numeros),
-    #                        promedio = Calculadora.promedio(Numeros),
-    #                        nMenor = Calculadora.num_Menor(Numeros),
-    #                        nMayor = Calculadora.num_Mayor(Numeros),
-    #                        name = "Resultado de " + str(len(Numeros)) + " números")
-
-
-if __name__ == "__main__":
-    app.run(debug = True)
+if __name__ == '__main__':
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.debug = True
+    app.run()
--- forms.py
+++ forms.py
@@ -3,10 +3,36 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,SubmitField,FieldList,FormField,SelectField,RadioField
 
 from wtforms.fields import EmailField
+from wtforms import validators
+
+def mi_validacion(form,field):
+    if len(field.data)==0:
+        raise validators.ValidationError('El campo no tiene datos')
 
 class UserForm(Form):
-    matricula=StringField('Matricula')
-    nombre=StringField('Nombre')
-    apaterno=StringField('Apaterno')
+    matricula = StringField('Matricula',
+    [validators.DataRequired(message='El campo es requerido'),
+     validators.length(min=5,max=10,message='Ingrese minimo 5 maximo 10')])
+    nombre=StringField('Nombre',
+    [validators.DataRequired(message='El campo nombre es requerido'),
+     validators.length(min=5,max=10,message='Ingrese minimo 5 maximo 10')])
+
+    apaterno=StringField('Apaterno',[mi_validacion])
     amaterno=StringField('Amaterno')
     email=EmailField('Correo')
+
+
+
+class LoginForm(Form):
+    matricula = StringField('usuario',
+    [validators.DataRequired(message='El campo usuario es requerido'),
+     validators.length(min=5,max=10,message='Ingrese minimo 5 maximo 10')])
+    nombre=StringField('Nombre',
+    [validators.DataRequired(message='El campo nombre es requerido'),
+     validators.length(min=5,max=10,message='Ingrese minimo 5 maximo 10')])
+
+    apaterno=StringField('Apaterno',[mi_validacion])
+    amaterno=StringField('Amaterno')
+    email=EmailField('Correo')
+    
+
